03fa432e998bafabc2ee266d7088ee408b37b8c2
Pedro Bueno <devbueno3@gmail.com>
2024-03-21 19:20:23 -0300

    v1 dessa página

--- src/pages/createNote.tsx
+++ src/pages/createNote.tsx
@@ -1,8 +1,36 @@
+import { useState } from "react"
+import { api } from "~/utils/api";
+
 export default function createNote(){
+
+    const [inputTitle, setTitle] = useState<string>('')
+    const [inputContent, setContent] = useState<string>('')
+    const { mutate: noteDB } = api.create.createNote.useMutation();
     return (
         <>
-            <h1>Saalve</h1>
-            <p>Aqui poderemos fazer altas coisas depois</p>
+            <div id="space_Write_Note" className="w-2/3">
+                <div id="save_Or_Delete">
+                  <button type='button'
+                    onClick={
+                      () => {
+                        noteDB({
+                          title: inputTitle,
+                          content: inputContent
+                        })
+                      }
+                    } 
+                  >Save the note</button>
+                  <span>...</span>
+                </div>
+                <input type="text" className="bg-blue-500"
+                  value={inputTitle}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea className="bg-green-500"
+                  value={inputContent}
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+              </div>
         </>
     )
 }
