7f0548f9be51c0db70dc6ec3597fedd9ba8a1d9e
Pedro Bueno <devbueno3@gmail.com>
2024-03-22 21:28:41 -0300

    Refatorei o componente button para ser ainda mais dinâmico

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,7 @@
 export default function Button(props: any){
     return (
         <>
-            <button type="button" onClick={props.onClick} className="bg-gray-500 border-2 w-48 mt-2">Write a new note</button>
+            <button type="button" onClick={props.onClick} className={props.className}>{props.value}</button>
         </>
     )
 }
--- src/pages/createNote.tsx
+++ src/pages/createNote.tsx
@@ -1,36 +1,40 @@
 import { useState } from "react"
 import { api } from "~/utils/api";
+import Button from "~/components/button";
 
 export default function createNote(){
 
     const [inputTitle, setTitle] = useState<string>('')
     const [inputContent, setContent] = useState<string>('')
-    const { mutate: noteDB } = api.create.createNote.useMutation();
+    const { mutate: noteDB } = api.personalNote.createNote.useMutation();
     return (
         <>
-            <div id="space_Write_Note" className="w-2/3">
+            <div id="space_Write_Note" className="flex flex-col w-screen h-screen bg-blue-100">
+              <div className=" m-3">
                 <div id="save_Or_Delete">
-                  <button type='button'
-                    onClick={
-                      () => {
-                        noteDB({
-                          title: inputTitle,
-                          content: inputContent
-                        })
-                      }
-                    } 
-                  >Save the note</button>
+                  <Button
+                    onClick={() => {  
+                      noteDB({
+                        title: inputTitle,
+                        content: inputContent
+                      })
+                    }} value="Save the note" className="bg-gray-500 border-2 w-48"
+                  />
+
                   <span>...</span>
                 </div>
-                <input type="text" className="bg-blue-500"
-                  value={inputTitle}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
-                <textarea className="bg-green-500"
-                  value={inputContent}
-                  onChange={(e) => setContent(e.target.value)}
-                ></textarea>
+                <div className="flex flex-col">
+                  <input type="text" className="bg-blue-500 h-[50px]"
+                    value={inputTitle}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <textarea className="bg-green-500 resize-none h-[300.5px]"
+                    value={inputContent}
+                    onChange={(e) => setContent(e.target.value)}
+                  ></textarea>
+                </div>
               </div>
+            </div>
         </>
     )
 }
