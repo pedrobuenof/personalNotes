ac1de2aeb739160d0cfde3a5c8dc9bd445533cdd
Pedro Bueno <devbueno3@gmail.com>
2024-03-29 01:18:04 -0300

    v1 layout página Criar Notas(Create Notes) finalizado + funcionalidade de navegar para página inicial(list notes)

--- src/pages/createNote.tsx
+++ src/pages/createNote.tsx
@@ -18,28 +18,39 @@ export default function createNote(){
     }
   }, [saveNote])
 
+  const backToListAllNotes = () => {
+    router.push('/')
+  }
     return (
         <>
-            <div id="space_Write_Note" className="flex flex-col w-screen h-screen bg-blue-100">
-              <div className=" m-3">
-                <div id="save_Or_Delete">
+            <div id="space_Write_Note" className="flex flex-col w-screen h-screen bg-[#E5E1DA] p-3">
+              <div className="flex flex-col justify-center items-center ">
+                <div id="save_Or_Delete" className="flex justify-between w-full px-2">
+                  <Button onClick={backToListAllNotes}
+                    value="All notes"/>
                   <Button
                     onClick={() => {  
                       noteDB({
                         title: inputTitle,
                         content: inputContent
                       })
-                    }} value="Save the note" className="bg-gray-500 border-2 w-48"
+                    }} value="Save"
                   />
                 </div>
-                <div className="flex flex-col">
-                  <input type="text" className="bg-blue-500 text-xl h-[50px] pl-3"
+                <div className="flex flex-col h-auto rounded-md p-1 bg-white w-full">
+                  <input type="text" className="text-xl h-[50px] pl-3 border-b-2"
                     value={inputTitle}
                     onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Title"
                   />
-                  <textarea className="bg-green-500 resize-none h-[300.5px] pl-3"
+                  <textarea className="resize-none h-auto min-h-[300px] max-h-[630px] py-3 pl-3 pr-2"
                     value={inputContent}
-                    onChange={(e) => setContent(e.target.value)}
+                    onChange={(e) => {
+                      setContent(e.target.value)
+                      e.target.style.height = 'auto';
+                      e.target.style.height = (e.target.scrollHeight) + 'px';
+                    }}
+                    placeholder="Note"
                   ></textarea>
                 </div>
               </div>
